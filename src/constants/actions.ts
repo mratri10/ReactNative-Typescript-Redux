2cf5892c8e9e30ff753d7ea02ec4d053a782327f
atri sinarmas <atri.alfa@sinarmasmining.com>
2022-06-09 07:58:32 +0700

    feat(navigation): between screeen and handle back screen

--- src/constants/actions.ts
+++ src/constants/actions.ts
@@ -1,3 +1,4 @@
-export const HOMETAB = "HOMETAB";
-export const PROFILETAB = "PROFILETAB";
-export const SCANTAB = "SCANTAB";
+export const HOMETAB = 'HOMETAB';
+export const PROFILETAB = 'PROFILETAB';
+export const SCANTAB = 'SCANTAB';
+export const ROUTE = 'ROUTE';
---
+++ src/ui/screen/checkout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function CheckoutScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Checkout</Text>
+    </View>
+  );
+}
+
+export default CheckoutScreen;
